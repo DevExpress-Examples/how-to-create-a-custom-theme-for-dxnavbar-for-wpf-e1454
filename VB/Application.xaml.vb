e91b05f16ce0ee3b976ae149926cc79dc06cea53
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/Application.xaml.vb
@@ -0,0 +1,20 @@
+﻿' Developer Express Code Central Example:
+' How to create a custom theme for DXNavBar for WPF
+' 
+' This is an example of applying a custom theme to the NavBarControl. A demo theme
+' is defined in CustomTheme.xaml. Its name is assigned to the NavBar's
+' ThemeManager.ThemeName.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E1454
+
+
+Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace DXNavBarCustomTheme
+
+	Partial Public Class App
+		Inherits Application
+	End Class
+End Namespace
